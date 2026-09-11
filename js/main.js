ead11208f29a671e2d89ee2ab28db8aed2901167
Michael Tchuindjang <39724203+Micdejc@users.noreply.github.com>
2026-09-11 22:19:47 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -359,9 +359,9 @@ function initialiseTerminalMode() {
    CONSOLE TYPING ANIMATION
 ========================================================= */
 
-const consoleText1 = "Cybersecurity thought leader & AI security researcher, <strong> UK Cyber Security Council Expert, Heads Talk AI & Cybersecurity Analyst, Startupbootcamp Mentor & Associate Lecturer at University of West of England </strong>";
+const consoleText1 = "Cybersecurity thought leader & AI security researcher, UK Cyber Security Council Expert, Heads Talk AI & Cybersecurity Analyst, Startupbootcamp Mentor & Associate Lecturer at University of West of England";
 
-const consoleText2 = "Researcher, educator and industry contributor specialising in <strong>LLM security, adversarial AI, multi-turn jailbreaking, representation engineering and AI governance </strong>, turning cutting-edge research into practical cybersecurity insight.";
+const consoleText2 = "Researcher, educator and industry contributor specialising in LLM security, adversarial AI, multi-turn jailbreaking, representation engineering and AI governance, turning cutting-edge research into practical cybersecurity insight.";
 
 let typingTimer = null;
 
