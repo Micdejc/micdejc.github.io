78973c14a6cda000e5d4fb5f04e394dacacf5ae9
Michael Tchuindjang <39724203+Micdejc@users.noreply.github.com>
2026-09-18 10:13:07 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,88 +2,28 @@
    GOOGLE CALENDAR CONFIGURATION
 ========================================================= */
 
+/* =========================================================
+   GOOGLE CALENDAR CONFIGURATION
+========================================================= */
+
 const GOOGLE_CALENDAR_CONFIG = {
 
     apiKey:
         "AIzaSyCBO1W-jM_XGtZUuNFrQDKD_E0bDy1NtNQ",
 
-    calendars: [
+    calendarId:
+        "en.uk#holiday@group.v.calendar.google.com",
 
-        {
-            id:
-                "en.uk#holiday@group.v.calendar.google.com",
+    calendarName:
+        "UK Holidays",
 
-            name:
-                "UK Holidays",
+    daysToLoad:
+        90,
 
-            type:
-                "uk"
+    maxResults:
+        20,
 
-        },
-
-        {
-            id:
-                "en-gb.christian#holiday@group.v.calendar.google.com",
-
-            name:
-                "Christian Holidays",
-
-            type:
-                "christian"
-
-        },
-
-        {
-            id:
-                "en.islamic#holiday@group.v.calendar.google.com",
-
-            name:
-                "Islamic Holidays",
-
-            type:
-                "islamic"
-
-        },
-
-        {
-            id:
-                "en.hinduism#holiday@group.v.calendar.google.com",
-
-            name:
-                "Hindu Holidays",
-
-            type:
-                "hindu"
-
-        },
-
-        {
-            id:
-                "en.judaism#holiday@group.v.calendar.google.com",
-
-            name:
-                "Jewish Holidays",
-
-            type:
-                "jewish"
-
-        },
-
-        {
-            id:
-                "en.orthodox_christianity#holiday@group.v.calendar.google.com",
-
-            name:
-                "Orthodox Christian Holidays",
-
-            type:
-                "orthodox"
-
-        }
-
-    ],
-
-    monthsToLoad:
-        12
+    cacheDuration:
+        60 * 60 * 1000
 
 };
