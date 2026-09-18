f5058a0655aebe2ef6571e5c7e9f2ac5b07c9124
Michael Tchuindjang <39724203+Micdejc@users.noreply.github.com>
2026-09-18 22:28:41 +0100

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -28,11 +28,11 @@ const UK_CALENDAR_CONFIG = {
 
 async function loadCalendarEvents() {
 
-  /* calendarEvents = [
+  calendarEvents = [
 
         {
             id: "test-my-day",
-            title: "MY Day",
+            title: "valentine",
             start: new Date(),
             end: new Date()
         }
@@ -45,7 +45,7 @@ async function loadCalendarEvents() {
     displayCalendarEvents();
     handleTodayHoliday();
 
-    return calendarEvents;  */
+    return calendarEvents;
 
     if (calendarDataLoaded) {
 
