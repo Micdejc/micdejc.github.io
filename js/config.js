f532ced9b450e02126bb534d74a68536359a24a8
Michael Tchuindjang <39724203+Micdejc@users.noreply.github.com>
2026-09-18 16:48:57 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,8 @@
    GOOGLE CALENDAR CONFIGURATION
 ========================================================= */
 
+/* API call example: https://www.googleapis.com/calendar/v3/calendars/en.uk%23holiday%40group.v.calendar.google.com/events?key=AIzaSyCBO1W-jM_XGtZUuNFrQDKD_E0bDy1NtNQ&singleEvents=true&orderBy=startTime&maxResults=10*/
+
 const GOOGLE_CALENDAR_CONFIG = {
 
     apiKey:
