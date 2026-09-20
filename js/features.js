5d772e903eac1f455b3f768df2455b8a46d023ff
Michael Tchuindjang <39724203+Micdejc@users.noreply.github.com>
2026-09-20 09:16:57 +0100

    Update features.js

--- js/features.js
+++ js/features.js
@@ -73,7 +73,7 @@
 
                     type:
                         "Report"
-                }
+                },
 
 
                 {
@@ -88,7 +88,7 @@
 
                     type:
                         "Report"
-                }
+                },
 
                 {
                     title:
