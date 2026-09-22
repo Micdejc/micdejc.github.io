d8b0a30e0e71aef04425c372716e2e4d1471abe3
Michael Tchuindjang <39724203+Micdejc@users.noreply.github.com>
2026-09-22 20:39:40 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -23,13 +23,17 @@
             id: "research",
             name: "Research"
         },
-        {
+/*        {
             id: "refusalguard",
             name: "RefusalGuard-M"
         },
         {
             id: "grammatical-mirage",
             name: "Grammatical Mirage"
+        },*/
+        {
+            id: "flagship",
+            name: "Flagship"
         },
         {
             id: "publications",
